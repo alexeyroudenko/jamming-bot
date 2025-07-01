b8d56758f27fb5bf1c8183283f52955c8dec958d
Alexey Roudenko <alexey.roudenko@gmail.com>
2025-07-01 10:14:06 +0300

    remove adapter, add docs

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,11 +1,6 @@
 
-// const url = document.location.protocol + "//" + document.location.hostname
-// const Url = url + ':5000'
-// const LegacyUrl = url + ':5000'
-// const CloudUrl = url + ":8003/api/v1/tags/tags/group/"
-
-const Url = 'https://jamming-bot.arthew0.online'
-const LegacyUrl = 'https://jamming-bot.arthew0.online/app/legacy/'
-const CloudUrl = "https://jamming-bot.arthew0.online/api/v1/tags/tags/group/"
-
+const url = document.location.protocol + "//" + document.location.hostname
+const Url = url + ':5000'
+const LegacyUrl = url + ':5000'
+const CloudUrl = url + ":8003/api/v1/tags/tags/group/"
 export { Url , LegacyUrl, CloudUrl}
