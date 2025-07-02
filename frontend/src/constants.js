79e57b142fc56c4743067080d22ecb3974c59696
Alexey Roudenko <alexey.roudenko@gmail.com>
2025-07-02 04:23:39 +0200

    https

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,13 @@
 
 const url = document.location.protocol + "//" + document.location.hostname
-const Url = url + ':5000'
-const LegacyUrl = url + ':5000'
-const CloudUrl = url + ":8003/api/v1/tags/tags/group/"
+
+// const Url = url + ':5000'
+// const LegacyUrl = url + ':5000'
+// const CloudUrl = url + ":8003/api/v1/tags/tags/group/"
+
+const Url = url + ''
+const LegacyUrl = url + '/app/legacy/'
+const CloudUrl = url + "/api/v1/tags/tags/group/"
 
 // const Url = 'https://jamming-bot.arthew0.online'
 // const LegacyUrl = 'https://jamming-bot.arthew0.online/app/legacy/'
