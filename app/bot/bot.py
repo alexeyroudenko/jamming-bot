f4312d4c5b586fca983ae444a17effb96273e07e
Alexey Roudenko <alexey.roudenko@gmail.com>
2025-08-16 06:23:11 +0200

    Add User-Agent header to HTTP requests in NetSpider class

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -534,7 +534,8 @@ class NetSpider():
                                             headers={
                                                     'Accept-Language': 'en-US, en;q=0.5',
                                                     'Accept-Charset':  'utf-8',
-                                                    'Accept-Encoding': 'gzip'
+                                                    'Accept-Encoding': 'gzip',
+                                                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
                                                 },
                                             timeout=3, 
                                             stream=True)                    
